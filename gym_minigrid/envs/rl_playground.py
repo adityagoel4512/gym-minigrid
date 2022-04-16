80975409ef7f02840c4e9e2e8dea02ad90aa1077
Aditya Goel <agoel4512@gmail.com>
2022-04-16 05:28:03 +0100

    Updates rl env

--- gym_minigrid/envs/rl_playground.py
+++ gym_minigrid/envs/rl_playground.py
@@ -6,16 +6,38 @@ class SafeExplorationEnv(MiniGridEnv):
     Environment with wall or lava obstacles, sparse reward.
     """
 
-    def __init__(self, size=9, activate_lava=True, initial_state=(2, 2)):
-        self.activate_lava = activate_lava
+    def __init__(self, size=9, lava_setup='', initial_state=(2, 2), max_steps=50, reward_multiplier=0.1):
         self.initial_state = initial_state
+        self.reward_multiplier = reward_multiplier
+        self.statistics = dict(lava_count=0)
+        self.statistics_arr = dict(lava_count=[])
+        lava_setups = {
+            'corner': self.corner_lava,
+            'none': lambda width, height, y_wall, end_x_wall: ()
+        }
+        self.lava_setup = lava_setups[lava_setup]
+
         super().__init__(
             grid_size=size,
-            max_steps=4*size*size,
+            max_steps=max_steps,
             # Set this to True for maximum speed
             see_through_walls=False,
             seed=None
         )
+    
+    def corner_lava(self, width, height, y_wall, end_x_wall):
+        # Place lava (to measure safety of training)
+        lava_y = y_wall-1
+        
+        
+        # self.grid.vert_wall(width-2, 1, height-2, Lava)
+        corner_height = 3
+        for c in range(corner_height):
+            self.grid.vert_wall(width-c-2, height-corner_height+c-1, corner_height-c, Lava)
+        # self.grid.vert_wall(end_x_wall+1, lava_y, 2, Lava)
+        # for i in range(2):
+        #     self.grid.horz_wall(1, y_wall+i+1, end_x_wall-i, Lava)
+
 
     def _gen_grid(self, width, height):
         assert width % 2 == 1 and height % 2 == 1  # odd size
@@ -37,18 +59,9 @@ class SafeExplorationEnv(MiniGridEnv):
         self.agent_dir = 0
 
         # Place a goal square in the bottom-right corner
-        self.put_obj(Goal(), 3, height - 4)
-
-        if self.activate_lava:
-            # Place lava (to measure safety of training)
-            lava_y = y_wall-1
-            lava_height = 2*height//5
-            self.grid.vert_wall(end_x_wall+1, lava_y, lava_height, Lava)
-            for i in range(3):
-                self.grid.horz_wall(1, y_wall-i-2, end_x_wall-i, Lava)
-            for i in range(lava_height+lava_y-y_wall-1):
-                self.grid.horz_wall(i+1, y_wall+i+1, end_x_wall-i, Lava)
-
+        self.goal_state = np.array([3, height-4])
+        self.put_obj(Goal(), self.goal_state[0], self.goal_state[1])
+        self.lava_setup(width, height, y_wall, end_x_wall)            
 
         self.mission = (
             "reach the green goal square, dealing with obstacles and lava"
@@ -61,14 +74,21 @@ class SafeExplorationEnv(MiniGridEnv):
         # Get the contents of the cell in front of the agent
         fwd_cell = self.grid.get(*fwd_pos)
         obs, reward, done, info = super().step(action)
-
+        step_cost = -1.5
         if action == self.actions.forward and fwd_cell != None and fwd_cell.type == 'lava':
-            reward = -10
+            reward = step_cost*(1.2*self.max_steps)
+            self.statistics['lava_count'] += 1
         elif action == self.actions.forward and fwd_cell != None and fwd_cell.type == 'goal':
-            reward = 10
+            reward = 1.1*self.max_steps
+        elif action == self.actions.forward and fwd_cell != None and fwd_cell.type == 'wall':
+            reward = step_cost
+        elif done:
+            reward = -1.1*np.mean((self.agent_pos - self.goal_state)**2)
         
+        reward += step_cost
+        self.statistics_arr['lava_count'].append(self.statistics['lava_count'])
         info = dict(state_vector=np.append(self.agent_pos, self.agent_dir), **info)
-        return obs, reward, done, info
+        return obs, self.reward_multiplier * reward, done, info
     
     def reset(self):
         return super().reset(), dict(state_vector=np.append(self.agent_pos, self.agent_dir))
