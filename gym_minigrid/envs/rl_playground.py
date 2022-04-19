edc57e5508e67b5502a12c9b43037342006f72a8
Aditya Goel <agoel4512@gmail.com>
2022-04-20 00:55:20 +0100

    Adds offline data collection

--- gym_minigrid/envs/rl_playground.py
+++ gym_minigrid/envs/rl_playground.py
@@ -1,22 +1,30 @@
+from operator import ne
+
+from zmq import device
 from gym_minigrid.minigrid import *
 from gym_minigrid.register import register
 import copy
+import torch
+from collections import namedtuple
 
+Transition = namedtuple('Transitions', ('state', 'action', 'reward', 'next_state'))
 class SafeExplorationEnv(MiniGridEnv):
     """
     Environment with wall or lava obstacles, sparse reward.
     """
 
-    def __init__(self, size=9, lava_setup='', initial_state=(2, 2), max_steps=50, reward_multiplier=0.1):
+    def __init__(self, size=9, lava_setup='', initial_state=(3, 2), max_steps=50, reward_multiplier=0.1, offline_regions=False):
         self.initial_state = initial_state
         self.reward_multiplier = reward_multiplier
         self.statistics = dict(lava_count=0)
+        self.offline_regions = offline_regions
         self.statistics_arr = dict(lava_count=[])
         lava_setups = {
             'corner': self.corner_lava,
             'wall': self.wall_lava,
             'none': lambda width, height, y_wall, end_x_wall: ()
         }
+        
         self.lava_setup = lava_setups[lava_setup]
 
         super().__init__(
@@ -27,7 +35,19 @@ class SafeExplorationEnv(MiniGridEnv):
             seed=None
         )
     
+    def offline_cells(self):
+        for row in range(self.height):
+            for col in range(self.width):
+                cell = self.grid.get(col, row)
+                if cell is None or cell.type == 'goal':
+                    yield col, row, cell
+
+    def setup_offline_regions(self):
+        for col, row, _ in self.offline_cells():
+            self.grid.set(col, row, Floor(color='blue'))
+    
     def corner_lava(self, width, height, y_wall, end_x_wall):
+
         # Place lava (to measure safety of training)
         lava_y = y_wall-1
         
@@ -45,7 +65,8 @@ class SafeExplorationEnv(MiniGridEnv):
         self.grid.horz_wall(1, y_wall-1, end_x_wall, Lava)
         self.grid.horz_wall(1, height//2, end_x_wall, Lava)
 
-    def _gen_grid(self, width, height):
+
+    def _gen_grid(self, width, height, state=None, dir=None):
         assert width % 2 == 1 and height % 2 == 1  # odd size
 
         # Create an empty grid
@@ -58,19 +79,20 @@ class SafeExplorationEnv(MiniGridEnv):
         end_x_wall = width//2
         y_wall = height//2
         self.grid.horz_wall(1, y_wall-1, end_x_wall, Wall)
-        self.grid.horz_wall(1, height//2, end_x_wall, Wall)
+        self.grid.horz_wall(1, y_wall, end_x_wall, Wall)
 
         # Place the agent in the top-left corner
-        self.agent_pos = np.array(self.initial_state)
-        self.agent_dir = 0
+        self.agent_pos = np.array(state or self.initial_state)
+        self.agent_dir = dir or 0
 
         # Place a goal square in the bottom-right corner
         self.goal_state = np.array([3, height-4])
         self.put_obj(Goal(), self.goal_state[0], self.goal_state[1])
-        self.lava_setup(width, height, y_wall, end_x_wall)            
-
+        self.lava_setup(width, height, y_wall, end_x_wall)  
+        if self.offline_regions:      
+            self.setup_offline_regions()
         self.mission = (
-            "reach the green goal square, dealing with obstacles and lava"
+            "reach the green goal square, dealing with obstacles and lava" if not self.offline_regions else "offline data regions"
         )
     
     def construct_state_vector(self, agent_pos, agent_dir):
@@ -82,6 +104,11 @@ class SafeExplorationEnv(MiniGridEnv):
         agent_dir -= 1
         return np.append(self.agent_pos, self.agent_dir)
     
+    def deconstruct_state_vector(self, state_vector):
+        pos = np.array([state_vector[0]*(self.width//2), state_vector[1]*(self.height//2)], dtype=np.int8)
+        dir = state_vector[2] + 1
+        return pos, dir
+
     def step(self, action):
         # Get the position in front of the agent
         fwd_pos = self.front_pos
@@ -89,9 +116,11 @@ class SafeExplorationEnv(MiniGridEnv):
         # Get the contents of the cell in front of the agent
         fwd_cell = self.grid.get(*fwd_pos)
         obs, reward, done, info = super().step(action)
+        reward = 0
         step_cost = 1
         if action == self.actions.forward and fwd_cell != None and fwd_cell.type == 'lava':
-            reward = -step_cost*(1.1*self.max_steps)
+            reward = -100
+            # reward = -10000
             self.statistics['lava_count'] += 1
         elif action == self.actions.forward and fwd_cell != None and fwd_cell.type == 'goal':
             reward = step_cost*self.max_steps*1.25
@@ -106,33 +135,26 @@ class SafeExplorationEnv(MiniGridEnv):
         self.statistics_arr['lava_count'].append(self.statistics['lava_count'])
         info = dict(state_vector=self.construct_state_vector(self.agent_pos, self.agent_dir), **info)
         return obs, self.reward_multiplier * reward, done, info
-    
+
     def reset(self):
         return super().reset(), dict(state_vector=self.construct_state_vector(self.agent_pos, self.agent_dir))
 
-class RLExperimentS21(SafeExplorationEnv):
-    def __init__(self):
-        super().__init__(size=21, activate_lava=True)
-
-class RLExperimentS21NoLava(SafeExplorationEnv):
-    def __init__(self):
-        super().__init__(size=21, activate_lava=False)
-
-class RLExperimentS51(SafeExplorationEnv):
-    def __init__(self):
-        super().__init__(size=51, activate_lava=True)
-
-register(
-    id='MiniGrid-RLExperimentS21-v0',
-    entry_point='gym_minigrid.envs:RLExperimentS21'
-)
-
-register(
-    id='MiniGrid-RLExperimentS21NoLava-v0',
-    entry_point='gym_minigrid.envs:RLExperimentS21NoLava'
-)
-
-register(
-    id='MiniGrid-RLExperimentS51-v0',
-    entry_point='gym_minigrid.envs:RLExperimentS51'
-)
+    def transitions_for_offline_data(self):
+        for col, row, cell in self.offline_cells():
+            print(col, row, cell)
+            for dir in range(4):
+                for action in range(3):
+                    print((col, row, dir))
+                    # sets grid world in desired state and direction
+                    self._gen_grid(self.width, self.height, (col, row), dir)
+                    state_vector = torch.from_numpy(np.concatenate(self.construct_state_vector(self.agent_pos, self.agent_dir), axis=None)).float().unsqueeze(0)
+                    # executes action, observes reward and next state
+                    _, reward, _, _ = self.step(action)
+                    new_cell = self.grid.get(self.agent_pos[0], self.agent_pos[1])
+                    if new_cell is None or new_cell.type != 'lava':
+                        next_state_trace = torch.from_numpy(np.concatenate(self.construct_state_vector(self.agent_pos, self.agent_dir), axis=None)).float().unsqueeze(0)
+                        assert next_state_trace is not None, ('not none!', Transition(state=state_vector, action=action, reward=reward, next_state=next_state_trace))
+                        reward = torch.tensor([[reward]])
+                        action = torch.tensor([[action]])
+                        print(Transition(state=state_vector, action=action, reward=reward, next_state=next_state_trace))
+                        yield Transition(state=state_vector, action=action, reward=reward, next_state=next_state_trace)
